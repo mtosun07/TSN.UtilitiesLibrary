60a010f9af988b7d3222b60696abe035466e87a7
Mustafa Tosun <www.mustafatosun.net>
2025-09-13 23:43:34 +0300

    Some improvements

--- TSN.UtilitiesLibrary/CommonEqualityComparer.cs
+++ TSN.UtilitiesLibrary/CommonEqualityComparer.cs
@@ -4,25 +4,25 @@
     {
         static CommonEqualityComparer()
         {
-            _default = new CommonEqualityComparer<T?>((x, y) => ReferenceEquals(x, y) || (x is null ? y is null : (y is not null && x.Equals(y))));
+            _default = new CommonEqualityComparer<T>((x, y) => ReferenceEquals(x, y) || (x is null ? y is null : (y is not null && x.Equals(y))));
         }
-        public CommonEqualityComparer(Func<T?, T?, bool>? equalityComparer, Func<T?, int>? hashCodeCalculator = null)
+        public CommonEqualityComparer(Func<T?, T?, bool>? equalityComparer, Func<T, int>? hashCodeCalculator = null)
         {
             _equalityComparer = equalityComparer ?? throw new ArgumentNullException(nameof(equalityComparer));
-            _hashCodeCalculator = hashCodeCalculator ?? new Func<T?, int>(o => o?.GetHashCode() ?? 0);
+            _hashCodeCalculator = hashCodeCalculator ?? new Func<T, int>(o => o?.GetHashCode() ?? 0);
         }
 
 
-        private static readonly CommonEqualityComparer<T?> _default;
+        private static readonly CommonEqualityComparer<T> _default;
         
         private readonly Func<T?, T?, bool> _equalityComparer;
-        private readonly Func<T?, int> _hashCodeCalculator;
+        private readonly Func<T, int> _hashCodeCalculator;
 
-        public static CommonEqualityComparer<T?> Default => _default;
+        public static CommonEqualityComparer<T> Default => _default;
 
 
 
         public bool Equals(T? x, T? y) => _equalityComparer.Invoke(x, y);
-        public int GetHashCode(T? obj) => _hashCodeCalculator.Invoke(obj);
+        public int GetHashCode(T obj) => _hashCodeCalculator.Invoke(obj);
     }
 }
